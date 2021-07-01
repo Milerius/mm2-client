13ac676541f4e2a3caa92e0644ebed350bab4b49
milerius <rmscastle@gmail.com>
2021-07-01 12:02:38 +0200

    feat(cli): use filter has prefix

--- cli/completer.go
+++ cli/completer.go
@@ -3,10 +3,11 @@ package cli
 import "github.com/c-bata/go-prompt"
 
 func Completer(t prompt.Document) []prompt.Suggest {
-	return []prompt.Suggest{
+	s := []prompt.Suggest{
 		{Text: "exit", Description: "Quit the application"},
 		{Text: "init", Description: "Init MM2 Dependencies, Download/Setup"},
 		{Text: "help <command>", Description: "Show the help for a specific command"},
 		{Text: "help", Description: "Show the global help"},
 	}
+	return prompt.FilterHasPrefix(s, t.GetWordBeforeCursor(), true)
 }
