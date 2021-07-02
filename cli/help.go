8a9bdf4e7b832e55e9b9b41289e7b9b93d600c30
milerius <rmscastle@gmail.com>
2021-07-02 18:26:19 +0200

    feat(help): better help

--- cli/help.go
+++ cli/help.go
@@ -1,17 +1,37 @@
 package cli
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/olekukonko/tablewriter"
+	"os"
+)
 
-const initHelp = `The init command allow you to bootstrap mm2 by downloading all the requirements
-usage: init`
+const initHelp = `The init command allow you to bootstrap mm2 by downloading all the requirements`
+
+const initUsage = `init`
+
+const exitHelp = `Quit the mm2-client CLI, doesn't shutdown mm2'`
+const exitUsage = `exit`
 
 func ShowGlobalHelp() {
-	fmt.Println("TODO: global help")
+	data := [][]string{
+		{"init", "", initHelp, initUsage},
+		{"exit", "", exitHelp, exitUsage},
+	}
+
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetAutoWrapText(false)
+	table.SetHeader([]string{"Command", "Args", "Description", "Usage"})
+	table.SetBorders(tablewriter.Border{Left: true, Top: false, Right: true, Bottom: false})
+	table.SetCenterSeparator("|")
+	table.AppendBulk(data) // Add Bulk Data
+	table.Render()
 }
 
 func ShowCommandHelp(command string) {
 	switch command {
 	case "init":
 		fmt.Println(initHelp)
+		fmt.Printf("usage %s\n", initUsage)
 	}
 }
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/dustin/go-humanize v1.0.0
 	github.com/kyokomi/emoji/v2 v2.2.8
 	github.com/manifoldco/promptui v0.8.0
+	github.com/olekukonko/tablewriter v0.0.5
 	github.com/schollz/progressbar/v3 v3.8.2
 	github.com/tyler-smith/go-bip39 v1.1.0
 )
