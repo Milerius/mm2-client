2bb577007e2c92e625a6137b2029d032ef53334b
milerius <rmscastle@gmail.com>
2021-07-03 20:54:11 +0200

    feat(disable_coin): add support for disable multiple coins

--- cli/disable_coin.go
+++ cli/disable_coin.go
@@ -1,6 +1,9 @@
 package cli
 
 import (
+	"encoding/json"
+	"fmt"
+	"mm2_client/config"
 	"mm2_client/helpers"
 	"mm2_client/http"
 )
@@ -13,5 +16,32 @@ func DisableCoin(coin string) {
 }
 
 func DisableCoins(coins []string) {
-
+	var outBatch []interface{}
+	for _, v := range coins {
+		if val, ok := config.GCFGRegistry[v]; ok {
+			outBatch = append(outBatch, http.NewDisableCoinRequest(val))
+		} else {
+			fmt.Printf("coin %s doesn't exist - skipping\n", v)
+		}
+	}
+	if len(outBatch) > 0 {
+		resp := http.BatchRequest(outBatch)
+		if len(resp) > 0 {
+			var outResp []http.DisableCoinAnswer
+			err := json.Unmarshal([]byte(resp), &outResp)
+			if err != nil {
+				fmt.Printf("Err: %v\n", err)
+			} else {
+				for _, cur := range outResp {
+					if len(cur.Error) == 0 {
+						helpers.PrintCheck(cur.Result.Coin+" successfully disabled", true)
+					} else {
+						fmt.Println(cur.Error)
+					}
+				}
+			}
+		}
+	} else {
+		fmt.Println("None of the desired coins exists - skipping")
+	}
 }
