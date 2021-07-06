d21496cc9d4eca84bd95a18833884135b06daa1b
milerius <rmscastle@gmail.com>
2021-07-06 18:58:31 +0200

    feat(completer): sort the completer

--- tooling/generate_helpers.go
+++ tooling/generate_helpers.go
@@ -4,14 +4,22 @@ import (
 	"fmt"
 	"mm2_client/config"
 	"mm2_client/http"
+	"sort"
 )
 
 func main() {
 	config.ParseDesktopRegistry(http.GetLastDesktopVersion())
+	keys := make([]string, 0, len(config.GCFGRegistry))
+
 	fmt.Println("var subCommandsEnable = []prompt.Suggest{")
 	for key, _ := range config.GCFGRegistry {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+	for _, key := range keys {
 		toPrint := fmt.Sprintf("{Text: %q, Description: \"Enable %s\"},", key, key)
 		fmt.Println(toPrint)
 	}
+
 	fmt.Println("}")
 }
