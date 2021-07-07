e7111495699e34de797c8b3656b9fe44b8404d1b
milerius <rmscastle@gmail.com>
2021-07-07 08:15:03 +0200

    feat(constants): add appdata path for future db integration

--- constants/constants.go
+++ constants/constants.go
@@ -1,6 +1,10 @@
 package constants
 
-import "mm2_client/helpers"
+import (
+	"mm2_client/helpers"
+	"os"
+	"runtime"
+)
 
 var (
 	GMM2Dir       = helpers.GetWorkingDir() + "/mm2"
@@ -8,3 +12,16 @@ var (
 	GMM2ConfPath  = GMM2Dir + "/MM2.json"
 	GMM2CoinsPath = GMM2Dir + "/coins.json"
 )
+
+func GetAppDataPath() string {
+	switch runtime.GOOS {
+	case "linux":
+		return os.Getenv("HOME") + "/atomicdex_cli"
+	case "darwin":
+		return os.Getenv("HOME") + "/Library/Application Support/atomicdex_cli"
+	case "windows":
+		return os.Getenv("APPDATA") + "/atomicdex_cli"
+	default:
+		return os.Getenv("HOME") + "/atomicdex_cli"
+	}
+}
