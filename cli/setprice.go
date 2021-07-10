552070cdfc82e0d68a5a219f337fff7b1ade1360
milerius <rmscastle@gmail.com>
2021-07-10 19:44:58 +0200

    feat(setprice): continue setprice interactive

--- cli/setprice.go
+++ cli/setprice.go
@@ -23,9 +23,37 @@ func retrieveCancelPrevious(base string, rel string) bool {
 	}
 }
 
+func askMinVolume(base string, volume *string, max *bool) *string {
+	curVol := "0"
+	if volume != nil {
+		curVol = *volume
+	}
+	if max != nil && *max {
+		curVol = http.MyBalance(base).Balance
+	}
+	return &curVol
+}
+
+func retrieveMinVolume(base string, volume *string, max *bool) *string {
+	prompt := promptui.Select{
+		Label: "Do you want to use a min_volume for (" + base + ") ?",
+		Items: []string{"Yes", "No"},
+	}
+	_, result, _ := prompt.Run()
+
+	fmt.Printf("You choose %q\n", result)
+
+	if result == "Yes" {
+		return askMinVolume(base, volume, max)
+	} else {
+		return nil
+	}
+}
+
 func interractiveSetPrice(base string, rel string, price string, volume *string, max *bool) {
 	cancelPrevious := retrieveCancelPrevious(base, rel)
-	if resp := http.SetPrice(base, rel, price, volume, max, cancelPrevious, nil, nil, nil, nil, nil); resp != nil {
+	minVolume := retrieveMinVolume(base, volume, max)
+	if resp := http.SetPrice(base, rel, price, volume, max, cancelPrevious, minVolume, nil, nil, nil, nil); resp != nil {
 		fmt.Println(resp)
 	}
 }
