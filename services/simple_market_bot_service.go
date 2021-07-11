a16487627c3fb62ca01ee5555cf21655596c0896
milerius <rmscastle@gmail.com>
2021-07-11 12:46:55 +0200

    feat(maker_bot): continue simple market maker bot

--- services/simple_market_bot_service.go
+++ services/simple_market_bot_service.go
@@ -4,8 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"mm2_client/constants"
-	"mm2_client/helpers"
+	"os"
+	"path/filepath"
 	"time"
 )
 
@@ -23,8 +25,26 @@ type SimplePairMarketMakerConf struct {
 	Enable         bool    `json:"enable"`
 }
 
+var (
+	WarningLogger *log.Logger
+	InfoLogger    *log.Logger
+	ErrorLogger   *log.Logger
+)
+
 var gSimpleMarketMakerRegistry = make(map[string]SimplePairMarketMakerConf)
 var gQuitMarketMakerBot chan bool
+
+func init() {
+	_ = os.MkdirAll(filepath.Join(constants.GetAppDataPath(), "logs"), os.ModePerm)
+	file, err := os.OpenFile(constants.GetAppDataPath()+"/logs/simple.market.maker.logs", os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	InfoLogger = log.New(file, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+	WarningLogger = log.New(file, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
+	ErrorLogger = log.New(file, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+}
 
 func NewMarketMakerConfFromFile(targetPath string) bool {
 	file, _ := ioutil.ReadFile(targetPath)
@@ -36,22 +56,32 @@ func NewMarketMakerConfFromFile(targetPath string) bool {
 	return true
 }
 
+func marketMakerProcess() {
+	InfoLogger.Println("process market maker")
+	for key, value := range gSimpleMarketMakerRegistry {
+		if value.Enable {
+			InfoLogger.Printf("Treating %s\n", key)
+		}
+	}
+}
+
 func StartSimpleMarketMakerBotService() {
 	for {
 		select {
 		case <-gQuitMarketMakerBot:
-			helpers.PrintCheck("Simple Market Maker Bot service successfully stopped", true)
+			InfoLogger.Println("Simple Market Maker Bot service successfully stopped")
 			constants.GSimpleMarketMakerBotRunning = false
 			close(gQuitMarketMakerBot)
 			return
 		default:
+			marketMakerProcess()
 			time.Sleep(30 * time.Second)
 		}
 	}
 }
 
 func StopSimpleMarketMakerBotService() {
-	fmt.Println("Stopping Simple Market Maker Bot Service - may take up to 30 seconds")
+	InfoLogger.Println("Stopping Simple Market Maker Bot Service - may take up to 30 seconds")
 	go func() {
 		gQuitMarketMakerBot <- true
 	}()
@@ -63,7 +93,7 @@ func StartSimpleMarketMakerBot() {
 			fmt.Println("Simple Market Maker bot is already running (or being stopped) - skipping")
 		} else {
 			if resp := NewMarketMakerConfFromFile(constants.GSimpleMarketMakerConf); resp {
-				fmt.Printf("Starting simple market maker bot with %d coin(s)\n", len(gSimpleMarketMakerRegistry))
+				InfoLogger.Printf("Starting simple market maker bot with %d coin(s)\n", len(gSimpleMarketMakerRegistry))
 				constants.GSimpleMarketMakerBotRunning = true
 				gQuitMarketMakerBot = make(chan bool)
 				go StartSimpleMarketMakerBotService()
