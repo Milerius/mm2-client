f77eb3c550f5764d66e5e9d72bbff65589e782e8
milerius <rmscastle@gmail.com>
2021-07-14 00:39:44 +0200

    feat(desktop): update desktop tests

--- config/desktop_test.go
+++ config/desktop_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestGetDesktopPath(t *testing.T) {
-	folderInfo, err := os.Stat(GetDesktopPath())
+	folderInfo, err := os.Stat(GetDesktopPath("standard"))
 	if os.IsNotExist(err) {
 		t.Errorf("Failed, expecting directory to exist: %v", err)
 	}
