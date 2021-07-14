64f8df3b9004164d7ef8812fe547a892b7fd7153
milerius <rmscastle@gmail.com>
2021-07-14 17:14:33 +0200

    feat(cancel_all_orders): continue cancel all orders

--- http/cancel_all_orders.go
+++ http/cancel_all_orders.go
@@ -8,16 +8,18 @@ import (
 	"net/http"
 )
 
+type DataCancel struct {
+	Base   *string `json:"base,omitempty"`
+	Rel    *string `json:"rel,omitempty"`
+	Ticker *string `json:"ticker,omitempty"`
+}
+
 type CancelAllOrdersRequest struct {
 	Userpass string `json:"userpass"`
 	Method   string `json:"method"`
 	CancelBy struct {
-		Type string `json:"type"`
-		Data *struct {
-			Base   *string `json:"base,omitempty"`
-			Rel    *string `json:"rel,omitempty"`
-			Ticker *string `json:"ticker,omitempty"`
-		} `json:"data,omitempty"`
+		Type string      `json:"type"`
+		Data *DataCancel `json:"data,omitempty"`
 	} `json:"cancel_by"`
 }
 
@@ -31,6 +33,16 @@ type CancelAllOrdersAnswer struct {
 func NewCancelAllOrdersRequest(kind string, args []string) *CancelAllOrdersRequest {
 	genReq := NewGenericRequest("cancel_all_orders")
 	req := &CancelAllOrdersRequest{Userpass: genReq.Userpass, Method: genReq.Method}
+	switch kind {
+	case "all":
+		req.CancelBy.Type = "All"
+	case "by_pair":
+		req.CancelBy.Type = "Pair"
+		req.CancelBy.Data = &DataCancel{Base: &args[0], Rel: &args[1]}
+	case "by_coin":
+		req.CancelBy.Type = "Coin"
+		req.CancelBy.Data = &DataCancel{Ticker: &args[0]}
+	}
 	return req
 }
 
