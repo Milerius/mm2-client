5c30961bf3295447321093a64cc0f79f577416ba
milerius <rmscastle@gmail.com>
2021-07-16 18:22:49 +0200

    feat(android): continue android support

--- go.mod
+++ go.mod
@@ -25,6 +25,7 @@ require (
 	github.com/ulule/limiter/v3 v3.8.0
 	github.com/valyala/fasthttp v1.28.0
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
+	golang.org/x/mobile v0.0.0-20210716004757-34ab1303b554 // indirect
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
--- mm2_tools_server/server.go
+++ mm2_tools_server/server.go
@@ -7,7 +7,6 @@ import (
 	mfasthttp "github.com/ulule/limiter/v3/drivers/middleware/fasthttp"
 	"github.com/ulule/limiter/v3/drivers/store/memory"
 	"github.com/valyala/fasthttp"
-	"log"
 	"runtime"
 )
 
@@ -19,18 +18,25 @@ func LaunchServer(appName string) {
 		glg.Info("Launch MM2 Tools Server from ios")
 	}
 
+	if runtime.GOOS == "android" {
+		glg.Get().SetMode(glg.STD)
+		glg.Info("Launch MM2 Tools Server from android")
+	}
+
 	gAppName = appName
 	router := InitRooter()
 	rate, err := limiter.NewRateFromFormatted("30-M")
 	if err != nil {
-		log.Fatal(err)
+		glg.Fatalf("error on limiter: %v", err)
 		return
 	}
 
 	store := memory.NewStore()
+	glg.Info("Memory store created")
 
 	// Create a fasthttp middleware.
 	middleware := mfasthttp.NewMiddleware(limiter.New(store, rate, limiter.WithTrustForwardHeader(true)))
+	glg.Info("Middleware created")
 
 	glg.Fatal(fasthttp.ListenAndServe(":"+fmt.Sprintf("%d", 1313), middleware.Handle(router.Handler)))
 }
