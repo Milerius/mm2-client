3f600c27fa7aca5abbe5faadf505a58e2a9cad45
milerius <rmscastle@gmail.com>
2021-07-17 11:36:55 +0200

    feat(cancel_order): add cancel_order for each platform

---
+++ mm2_tools_generics/mm2_wasm_request/cancel_order_arm64.go
@@ -0,0 +1,10 @@
+package mm2_wasm_request
+
+import (
+	"errors"
+	"mm2_client/mm2_tools_generics/mm2_data_structure"
+)
+
+func CancelOrder(uuid string) (*mm2_data_structure.CancelOrderAnswer, error) {
+	return nil, errors.New("not implemented")
+}
