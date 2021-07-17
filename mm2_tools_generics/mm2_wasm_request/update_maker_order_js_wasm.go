e3ddbdc2192a6af1984e17c75aaf8fa04236ce45
milerius <rmscastle@gmail.com>
2021-07-17 11:30:22 +0200

    feat(update_maker_order): fix wasm compilation

--- mm2_tools_generics/mm2_wasm_request/update_maker_order_js_wasm.go
+++ mm2_tools_generics/mm2_wasm_request/update_maker_order_js_wasm.go
@@ -1,6 +1,9 @@
 package mm2_wasm_request
 
-import "mm2_client/mm2_tools_generics/mm2_data_structure"
+import (
+	"errors"
+	"mm2_client/mm2_tools_generics/mm2_data_structure"
+)
 
 func UpdateMakerOrder(uuid string, newPrice *string, volumeDelta *string, max *bool, minVolume *string,
 	baseConfs *int, baseNota *bool, relConfs *int, relNota *bool) (*mm2_data_structure.UpdateMakerOrderAnswer, error) {
