03fbef6c1ba3433b358dfaf12b2b1200546bf414
milerius <rmscastle@gmail.com>
2021-07-17 09:45:18 +0200

    feat(wasm): empty my balance arm

--- mm2_tools_generics/mm2_wasm_request/my_balance_arm64.go
+++ mm2_tools_generics/mm2_wasm_request/my_balance_arm64.go
@@ -1,5 +1,7 @@
 package mm2_wasm_request
 
+import "mm2_client/mm2_tools_generics/mm2_data_structure"
+
 func MyBalance(coin string) (*mm2_data_structure.MyBalanceAnswer, error) {
 	return nil, nil
 }
