a32d5d9fe9e08a064a874bfdd568a0f6e87e9413
milerius <rmscastle@gmail.com>
2021-07-19 21:26:23 +0200

    feat(disable_coin): add disable coin data structure

---
+++ mm2_tools_generics/mm2_data_structure/disable_coin.go
@@ -0,0 +1,38 @@
+package mm2_data_structure
+
+import (
+	"encoding/json"
+	"fmt"
+	"mm2_client/config"
+	http2 "mm2_client/http"
+)
+
+type DisableCoinRequest struct {
+	Userpass string `json:"userpass"`
+	Method   string `json:"method"`
+	Coin     string `json:"coin"`
+}
+
+type DisableCoinAnswer struct {
+	Result struct {
+		CancelledOrders []string `json:"cancelled_orders,omitempty"`
+		Coin            string   `json:"coin"`
+	} `json:"result,omitempty"`
+	Error string `json:"error,omitempty"`
+}
+
+func NewDisableCoinRequest(cfg *config.DesktopCFG) *DisableCoinRequest {
+	genReq := http2.NewGenericRequest("disable_coin")
+	req := &DisableCoinRequest{Userpass: genReq.Userpass, Method: genReq.Method}
+	req.Coin = cfg.Coin
+	return req
+}
+
+func (req *DisableCoinRequest) ToJson() string {
+	b, err := json.Marshal(req)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	return string(b)
+}
