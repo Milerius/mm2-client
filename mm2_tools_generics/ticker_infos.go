76be4b7c5a6dd99e7f5ea5cb8f4bf3573a41447f
milerius <rmscastle@gmail.com>
2021-07-20 18:25:08 +0200

    feat(ticker_infos): more specific ticker_infos

--- mm2_tools_generics/ticker_infos.go
+++ mm2_tools_generics/ticker_infos.go
@@ -11,11 +11,12 @@ type TickerInfosRequest struct {
 }
 
 type TickerInfosAnswer struct {
-	Ticker      string `json:"ticker"`
-	LastPrice   string `json:"last_price"`
-	LastUpdated string `json:"last_updated"`
-	Volume24h   string `json:"volume24h"`
-	Provider    string `json:"provider"`
+	Ticker         string `json:"ticker"`
+	LastPrice      string `json:"last_price"`
+	LastUpdated    string `json:"last_updated"`
+	Volume24h      string `json:"volume24h"`
+	PriceProvider  string `json:"price_provider"`
+	VolumeProvider string `json:"volume_provider"`
 }
 
 func (req *TickerInfosAnswer) ToJson() string {
@@ -38,6 +39,6 @@ func (req *TickerInfosAnswer) ToWeb() map[string]interface{} {
 
 func GetTickerInfos(ticker string) *TickerInfosAnswer {
 	val, date, provider := services.RetrieveUSDValIfSupported(ticker)
-	volume, _, _ := services.RetrieveVolume24h(ticker)
-	return &TickerInfosAnswer{Ticker: ticker, LastPrice: val, LastUpdated: date, Provider: provider, Volume24h: volume}
+	volume, _, volumeProvider := services.RetrieveVolume24h(ticker)
+	return &TickerInfosAnswer{Ticker: ticker, LastPrice: val, LastUpdated: date, PriceProvider: provider, Volume24h: volume, VolumeProvider: volumeProvider}
 }
