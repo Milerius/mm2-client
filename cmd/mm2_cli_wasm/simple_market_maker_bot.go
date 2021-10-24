a99b72f4139cd31883b14bc22080c92271176817
milerius <rmscastle@gmail.com>
2021-10-24 15:51:26 +0200

    feat(wasm): add wasm part for the bot

--- cmd/mm2_cli_wasm/simple_market_maker_bot.go
+++ cmd/mm2_cli_wasm/simple_market_maker_bot.go
@@ -2,9 +2,35 @@ package main
 
 import (
 	"github.com/kpango/glg"
+	"mm2_client/config"
 	"mm2_client/market_making"
+	"mm2_client/mm2_tools_generics"
 	"syscall/js"
 )
+
+func startSimpleMarketMakerBot() js.Func {
+	jsfunc := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		go func() {
+			err := config.ParseMarketMakerConfFromUrl("http://localhost:8080/static/assets/mm2_market_maker.json")
+			if err != nil {
+				_ = glg.Errorf("err when starting simple market maker bot: %v", err)
+			}
+			mm2_tools_generics.StartSimpleMarketMakerBotCLI()
+		}()
+		return "done"
+	})
+	return jsfunc
+}
+
+func stopSimpleMarketMakerBot() js.Func {
+	jsfunc := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		go func() {
+			mm2_tools_generics.StopSimpleMarketMakerBotCLI()
+		}()
+		return "done"
+	})
+	return jsfunc
+}
 
 func startSimpleMarketMakerBotV1() js.Func {
 	jsfunc := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
--- mm2_tools_generics/mm2_wasm_request/start_simple_market_maker_bot_js_wasm.go
+++ mm2_tools_generics/mm2_wasm_request/start_simple_market_maker_bot_js_wasm.go
@@ -1,8 +1,25 @@
 package mm2_wasm_request
 
-import "mm2_client/mm2_tools_generics/mm2_data_structure"
+import (
+	"encoding/json"
+	"github.com/kpango/glg"
+	"mm2_client/mm2_tools_generics/mm2_data_structure"
+	"syscall/js"
+)
 import "errors"
 
 func StartSimpleMarketMakerBot() (*mm2_data_structure.StartSimpleMarketMakerAnswer, error) {
-	return nil, errors.New("not implemented yet")
+	req := mm2_data_structure.NewStartSimpleMarketMakerRequest().ToJson()
+	balVal, errVal := Await(js.Global().Call("rpc_request", req))
+	if errVal != nil {
+		return nil, errors.New(errVal[0].String())
+	} else {
+		var answer = &mm2_data_structure.StartSimpleMarketMakerAnswer{}
+		decodeErr := json.Unmarshal([]byte(balVal[0].String()), answer)
+		if decodeErr != nil {
+			_ = glg.Errorf("err: %v", decodeErr)
+			return nil, decodeErr
+		}
+		return answer, nil
+	}
 }
