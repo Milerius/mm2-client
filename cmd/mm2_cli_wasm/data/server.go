2877d23eb3c5667258635f587fbbf459d0f618ae
milerius <rmscastle@gmail.com>
2021-10-24 15:11:03 +0200

    feat(server): add a miniserver

---
+++ cmd/mm2_cli_wasm/data/server.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("./"))
+	http.Handle("/", fs)
+
+	log.Println("Listening on :8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
